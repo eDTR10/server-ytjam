2ae0dd28239dd4e4558e7c00ff2db713c8c72852
Zer0 <iannicocaulin@gmail.com>
2026-04-06 18:27:18 +0800

    fix: remove duplicate main block causing startup crash

--- server.py
+++ server.py
@@ -151,7 +151,3 @@ def on_playback_sync(data):
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     socketio.run(app, host='0.0.0.0', port=port, debug=False)
-
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
